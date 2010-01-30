abe9f4347debb2a0a69acde8721f14f64644c48a
Glen Maddern <glenmaddern@gmail.com>
2010-01-30 16:53:42 +1100

    glen is regarded. compiling now

--- src/main/scala/scorpio/GaeCheckLoggedIn.scala
+++ src/main/scala/scorpio/GaeCheckLoggedIn.scala
@@ -5,14 +5,14 @@ import scalaz.http.response.Response
 import collection.immutable.Stream
 import com.google.appengine.api.users.UserServiceFactory
 
-object GaeCheckLoggedIn extends RequestMangler {
+object GaeCheckLoggedIn extends MaybeHandler {
   val userService = UserServiceFactory.getUserService
 
   def apply(request: Request[Stream]) = {
-//    implicit val r = request
+    implicit val r = request
     userService.getCurrentUser match {
-      case null => Response.redirects(userService.createLoginURL("/"))
-      case _ => request
+      case null => Some(Response.redirects(userService.createLoginURL("/")))
+      case _ => None
     }
   }
 }
